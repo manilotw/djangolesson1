a85b0f47c2e759f57fedb75f6101b72e3ab02bd1
Amirzhan Albariyev <albariev123@gmail.com>
2025-02-05 16:58:40 +0300

    Add blank start page

---
+++ where_to_go/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.template import loader
+
+
+def show_map(request):
+    template = loader.get_template('main.html')
+    context = {}
+    rendered_page = template.render(context, request)
+
+    return HttpResponse(rendered_page)
