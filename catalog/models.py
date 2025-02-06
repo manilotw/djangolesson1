53bcc015f3645e69062551a0e3070524ff24e9e7
Amirzhan Albariyev <albariev123@gmail.com>
2025-02-06 18:47:48 +0300

    Add Location model

--- catalog/models.py
+++ catalog/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Location(models.Model):
+    title = models.CharField('Название локаций', max_length=200)
+    description_short = models.TextField('Короткое описание')
+    description_long = models.TextField('Длинное описание')
+    lng = models.FloatField('Координация 1')
+    lat = models.FloatField('Координация 2')
+
+    def __str__(self):
+        return self.title
