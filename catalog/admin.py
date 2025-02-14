d5fc0f5de19419b99e0f3910b210b44c163eb339
Amirzhan Albariyev <albariev123@gmail.com>
2025-02-14 23:36:32 +0300

    add peview

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
@@ -6,6 +7,15 @@ from .models import Location, Image
 
 class ImageInline(admin.TabularInline):
     model = Image
+
+    readonly_fields = ["image"]
+
+    readonly_fields = ["image_preview"]
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="100" height="100" />')
+        return "(No image)"
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
