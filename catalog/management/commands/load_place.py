4c7aaeb25fb9ee4d8ce32e7f4fa0eab5051d9533
Amirzhan Albariyev <albariev123@gmail.com>
2025-02-15 14:49:49 +0300

    add django command

---
+++ catalog/management/commands/load_place.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
+from catalog.models import Image, Location
+
+import os
+import requests
+
+
+
+class Command(BaseCommand):
+    help = 'Загрузка данных с JSON'
+
+    def add_arguments(self, parser):
+        parser.add_argument('data_url', type=str, help='Введите ссылку на JSON')
+
+    def handle(self, *args, **kwargs):
+        
+        def upload_image(place, image_url):
+            
+            response = requests.get(image_url)
+            response.raise_for_status
+            image_name = os.path.basename(image_url)
+            Image.objects.create(location=place, image=ContentFile(response.content, name=image_name))
+        url = kwargs['data_url']
+        response = requests.get(url)
+        response.raise_for_status()
+        answer = response.json()
+
+        title = answer['title']
+        images_url = answer['imgs']
+        description_short = answer['description_short']
+        description_long = answer['description_long']
+        lng = answer['coordinates']['lng']
+        lat = answer['coordinates']['lat']
+
+        place, created = Location.objects.get_or_create(
+            title=title,
+            lat=lat,
+            lng=lng,
+            defaults={
+                'description_short': description_short,
+                'description_long': description_long,
+            }
+        )
+
+        for image_url in images_url:
+            upload_image(place, image_url)
+
--- catalog/models.py
+++ catalog/models.py
@@ -14,14 +14,14 @@ class Location(models.Model):
     
 class Image(models.Model):
     location = models.ForeignKey(Location,default=0, on_delete=models.CASCADE, related_name='img')
-    number = models.IntegerField('Номер для порядка')
+    number = models.IntegerField('Номер для порядка', default=0, blank=True)
     image = models.ImageField('Фотография')
 
     class Meta:
         ordering = ['number']
 
     def __str__(self):
-        return f"{self.number}, {self.location.title}"
+        return f"{self.number} {self.location.title}"
 
 
 
