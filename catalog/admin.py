45eb3b9980e87aaa82c20ee0a2ff7aba39543947
Amirzhan Albariyev <albariev123@gmail.com>
2025-02-16 15:40:24 +0300

    add raw_id_fields

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminMixin
+from adminsortable2.admin import SortableInlineAdminMixin
 
 from .models import Location, Image
 
 
 class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
-
     readonly_fields = ["image_preview"]
     extra = 1
     fields = ["image", "location", "image_preview", "number"]
@@ -16,7 +15,7 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
 
     def image_preview(self, obj):
         if obj.image:
-            return format_html('<img src="{}" max-width="100" max-height="100" />', obj.image.url)
+            return format_html('<img src="{}" style="max-width: 100px; max-height: 100px;" />', obj.image.url)
         return "(No image)"
 
 
@@ -25,4 +24,16 @@ class LocationAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
 
 
-admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["id", "image_preview", "location"]
+    search_fields = ["location__title"]
+    list_filter = ["location"]
+    raw_id_fields = ["location"]  # Оптимизация выбора локации при большом количестве данных
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-width: 100px; max-height: 100px;" />', obj.image.url)
+        return "(No image)"
+
+    image_preview.short_description = "Preview"
