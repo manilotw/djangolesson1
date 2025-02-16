413f553c35b47b5c4cd55a008ceb1158e589a272
Amirzhan Albariyev <albariev123@gmail.com>
2025-02-16 15:14:28 +0300

    formated pep-8

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,24 +4,25 @@ from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminMixin
 
 from .models import Location, Image
 
+
 class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
 
     readonly_fields = ["image_preview"]
     extra = 1
-    fields = ['image', 'location', 'image_preview', 'number']
-    ordering = ['number']
-    sortable_field_name = 'number'
+    fields = ["image", "location", "image_preview", "number"]
+    ordering = ["number"]
+    sortable_field_name = "number"
 
     def image_preview(self, obj):
         if obj.image:
             return mark_safe(f'<img src="{obj.image.url}" width="100" height="100" />')
         return "(No image)"
 
+
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    inlines = [
-        ImageInline,
-    ]
+    inlines = [ImageInline]
+
 
 admin.site.register(Image)
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,13 @@
 import json
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.urls import reverse
 
-from where_to_go import settings
 from catalog.models import Location
 
-def show_map(request):
 
+def show_map(request):
     locations = Location.objects.all()
 
     features = [
@@ -16,41 +15,40 @@ def show_map(request):
             "type": "Feature",
             "geometry": {
                 "type": "Point",
-                "coordinates": [location.lng, location.lat]
+                "coordinates": [location.lng, location.lat],
             },
             "properties": {
                 "title": location.title,
                 "placeId": location.id,
-                "detailsUrl": reverse('get_place', kwargs={'place_id': location.id})
-            }
+                "detailsUrl": reverse("get_place", kwargs={"place_id": location.id}),
+            },
         }
         for location in locations
     ]
 
     locations_geojson = {
         "type": "FeatureCollection",
-        "features": features
+        "features": features,
     }
 
-    return JsonResponse(locations_geojson, json_dumps_params={"ensure_ascii": False, "indent": 2})
+    return JsonResponse(
+        locations_geojson,
+        json_dumps_params={"ensure_ascii": False, "indent": 2},
+    )
 
 
 def place(request, place_id):
-
     location = get_object_or_404(Location, id=place_id)
 
     place = {
-    "title": location.title,
-    "imgs": [],
-    "description_short": location.description_short,
-    "description_long": location.description_long,
-    "coordinates": {
-        "lat": location.lat,
-        "lng": location.lng
+        "title": location.title,
+        "imgs": [str(img.image.url) for img in location.img.all()],
+        "description_short": location.description_short,
+        "description_long": location.description_long,
+        "coordinates": {
+            "lat": location.lat,
+            "lng": location.lng,
+        },
     }
-}
-    for img in location.img.all():
-        place['imgs'].append(str(img.image.url))
 
-
-    return JsonResponse(place, safe=False, json_dumps_params={'indent': 2, 'ensure_ascii': False})
+    return JsonResponse(place, safe=False, json_dumps_params={"indent": 2, "ensure_ascii": False})
