6af93f0ba269e630af71642859a8c5e7e1a2117c
Amirzhan Albariyev <albariev123@gmail.com>
2025-02-18 16:23:38 +0300

    add try-except

--- catalog/management/commands/load_place.py
+++ catalog/management/commands/load_place.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import time
+import sys
 
 from django.core.management.base import BaseCommand
 from django.core.files.base import ContentFile
@@ -19,14 +21,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         def upload_image(place, image_url):
-            response = requests.get(image_url)
-            response.raise_for_status()
-            image_name = os.path.basename(image_url)
-
-            Image.objects.create(
-                location=place,
-                image=ContentFile(response.content, name=image_name),
-            )
+            try:
+                response = requests.get(image_url)
+                response.raise_for_status()
+                image_name = os.path.basename(image_url)
+                Image.objects.create(
+                    location=place,
+                    image=ContentFile(response.content, name=image_name),
+                )
+            except requests.exceptions.HTTPError as err:
+                print(f"Ошибка HTTP при загрузке {image_url}: {err}", file=sys.stderr)
+            except requests.exceptions.ConnectionError:
+                print(f"Проблемы с сетью при загрузке {image_url}. Повтор через 5 секунд.", file=sys.stderr)
+                time.sleep(5)
+                upload_image(place, image_url)
 
         url = kwargs["data_url"]
         response = requests.get(url)
