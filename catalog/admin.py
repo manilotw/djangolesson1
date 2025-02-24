ac324a6a16e9ec70e343cfe917557bcdc651ca3c
Amirzhan Albariyev <albariev123@gmail.com>
2025-02-24 19:00:20 +0300

    import sortable admin base

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from adminsortable2.admin import SortableInlineAdminMixin
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
 
 from .models import Location, Image
 
